530f0b5476b6b5be469b4f14ad5b099b5a6f1cfa
Sharon Colson <sharon.colson@gmail.com>
2023-11-25 14:52:07 -0600

    remove some unnecessary visualizations and organize images

---
+++ Model_logistic_reg_w_SMOTE.R
@@ -0,0 +1,87 @@
+library(tidymodels)
+library(imbalance)
+df <- read.csv("feature_engineering/train_data_model.csv",row.names = "X")
+df
+summary(df)
+glimpse(df)
+
+# Increases font size for all ggplot2 plots
+theme_set(theme_gray(base_size=18))
+# Increases font size for confusion matrix plots
+update_geom_defaults("text", list(size=6))
+
+options(repr.plot.width=6, repr.plot.height=5)
+
+#df$fraud_flag <- as.factor(df$fraud_flag)
+#Split data
+set.seed(20231124)
+data_split <- initial_split(df, prop = 0.7)
+train_data <- training(data_split)
+test_data <- testing(data_split)
+
+# Print the shape of original (imbalanced) training dataset
+train_y_categ <- train_data %>%
+  select(fraud_flag) %>%
+  table
+message(
+  paste0(
+    "Original dataset shape ",
+    paste(names(train_y_categ), train_y_categ, sep = ": ", collapse = ", ")
+  )
+)
+
+# Resample the training dataset using SMOTE
+smote_train_df <- train_data %>%
+  mutate(fraud_flag = factor(fraud_flag)) %>%
+  oversample(ratio = 0.99, method = "SMOTE", classAttr = "fraud_flag") %>%
+  mutate(fraud_flag = as.integer(fraud_flag))
+
+# Print the shape of resampled (balanced) training dataset
+smote_train_y_categ <- smote_train_df %>%
+  select(fraud_flag) %>% table
+message(
+  paste0(
+    "Resampled dataset shape ",
+    paste(names(smote_train_y_categ), smote_train_y_categ, sep = ": ", collapse = ", ")
+  )
+)
+smote_train_df$fraud_flag <- as.factor(smote_train_df$fraud_flag)
+smote_train_y_categ
+
+smote_train_df
+glimpse(smote_train_df)
+
+# Fit logistic regression model
+logistic_model <- glm(fraud_flag ~ ., data = smote_train_df, family = binomial)
+
+
+
+
+# Print a summary of the model
+summary(logistic_model)
+
+
+# Make predictions on the test set
+test_data$predicted_prob <- predict(logistic_model, newdata = test_data, type = "response")
+
+# Convert predicted probabilities to predicted class labels (0 or 1)
+test_data$predicted_class <- ifelse(test_data$predicted_prob > 0.5, 1, 0)
+
+# Create confusion matrix
+conf_matrix <- table(test_data$fraud_flag, test_data$predicted_class)
+
+# Print the confusion matrix
+conf_matrix
+
+# Compute metrics
+accuracy <- sum(diag(conf_matrix)) / sum(conf_matrix)
+precision <- conf_matrix[2, 2] / sum(conf_matrix[, 2])
+recall <- conf_matrix[2, 2] / sum(conf_matrix[2, ])
+f1_score <- 2 * (precision * recall) / (precision + recall)
+
+# Print metrics
+cat("Accuracy:", accuracy, "\n")
+cat("Precision:", precision, "\n")
+cat("Recall:", recall, "\n")
+cat("F1 Score:", f1_score, "\n")
+
--- classification_trees_SMOTE.R
+++ classification_trees_SMOTE.R
@@ -3,11 +3,11 @@ library(dplyr)
 library(tidymodels)
 library(ggplot2)
 library(imbalance)
+library(randomForest)
 
 
 
-
-df <- read.csv("feature_engineering/train_data.csv",row.names = "X")
+df <- read.csv("feature_engineering/train_data_model.csv",row.names = "X")
 df
 
 glimpse(df)
@@ -20,8 +20,8 @@ update_geom_defaults("text", list(size=6))
 
 options(repr.plot.width=6, repr.plot.height=5)
 
-#df$fraud_flag <- as.factor(df$fraud_flag)
-#Split data
+
+# Split data
 set.seed(20231124)
 data_split <- initial_split(df, prop = 0.7)
 train_data <- training(data_split)
@@ -42,7 +42,9 @@ message(
 smote_train_df <- train_data %>%
   mutate(fraud_flag = factor(fraud_flag)) %>%
   oversample(ratio = 0.99, method = "SMOTE", classAttr = "fraud_flag") %>%
-  mutate(fraud_flag = as.integer(fraud_flag))
+  mutate(fraud_flag = as.integer(fraud_flag)) %>%
+  mutate(fraud_flag = as.factor(fraud_flag)) %>%
+  mutate(fraud_flag = forcats::fct_recode(fraud_flag, "0" = "1", "1" = "2"))
 
 # Print the shape of resampled (balanced) training dataset
 smote_train_y_categ <- smote_train_df %>%
@@ -56,34 +58,157 @@ message(
 smote_train_df$fraud_flag <- as.factor(smote_train_df$fraud_flag)
 smote_train_y_categ
 
-smote_train_df
-glimpse(smote_train_df)
+# Define the random forest model with a progress bar and time estimate
+start_time <- Sys.time()
 
+rf_model <- rand_forest(trees = 100, mode = "classification") %>%
+  set_engine("randomForest", importance = TRUE)
 
+# Display progress and estimated time remaining
+for (i in 1:100) {
+  if (i %% 10 == 0) {
+    current_time <- Sys.time()
+    elapsed_time <- difftime(current_time, start_time, units = "secs")
+    trees_per_sec <- i / as.numeric(elapsed_time)
+    remaining_time <- (100 - i) / trees_per_sec
 
+    cat(sprintf("\r%d trees fitted, Estimated time remaining: %.2f seconds", i, remaining_time))
+    flush.console()
+  }
 
+  # Fit additional trees to the existing model
+  rf_model <- randomForest(fraud_flag ~ ., data = smote_train_df, ntree = i, keep.forest = TRUE,
+                           init.forest = rf_model$forest)
+}
 
-# Define the recipe
-tree_recipe <- recipe(fraud_flag ~ ., data = smote_train_df) %>%
-  step_dummy(all_nominal(), one_hot = TRUE)
-
-# Define the model specification
-tree_spec <- decision_tree(mode = "classification") %>%
-  set_engine("rpart")
-
-# Combine recipe and model specification into a workflow
-tree_workflow <- workflow() %>%
-  add_recipe(tree_recipe) %>%
-  add_model(tree_spec)
-
-# Train the model
-tree_fit <- tree_workflow %>%
-  fit(data = smote_train_df)
+cat("\n")  # Add a newline after the progress bar
 
 # Make predictions on the test set
-predictions <- predict(tree_fit, test_data) %>%
+test_data <- testing(data_split)
+rf_preds <- predict(fitted_model, new_data = test_data) %>%
   bind_cols(test_data)
 
 # Evaluate the model
-conf_matrix <- conf_mat(predictions, truth = fraud_flag, estimate = .pred_class)
-print(conf_matrix)
+rf_metrics <- rf_preds %>%
+  metrics(truth = fraud_flag, estimate = .pred_class)
+
+# Calculate Root Mean Squared Error (RMSE)
+rmse <- sqrt(mean((rf_preds$fraud_flag - rf_preds$.pred_class)^2))
+
+# Print RMSE
+cat("Root Mean Squared Error (RMSE):", rmse, "\n")
+
+# Print the evaluation metrics
+rf_metrics
+
+
+# #df$fraud_flag <- as.factor(df$fraud_flag)
+# #Split data
+# set.seed(20231124)
+# data_split <- initial_split(df, prop = 0.7)
+# train_data <- training(data_split)
+# test_data <- testing(data_split)
+
+# # Print the shape of original (imbalanced) training dataset
+# train_y_categ <- train_data %>%
+#   select(fraud_flag) %>%
+#   table
+# message(
+#   paste0(
+#     "Original dataset shape ",
+#     paste(names(train_y_categ), train_y_categ, sep = ": ", collapse = ", ")
+#   )
+# )
+
+# # Resample the training dataset using SMOTE
+# smote_train_df <- train_data %>%
+#   mutate(fraud_flag = factor(fraud_flag)) %>%
+#   oversample(ratio = 0.99, method = "SMOTE", classAttr = "fraud_flag") %>%
+#   mutate(fraud_flag = as.integer(fraud_flag))
+
+# # Print the shape of resampled (balanced) training dataset
+# smote_train_y_categ <- smote_train_df %>%
+#   select(fraud_flag) %>% table
+# message(
+#   paste0(
+#     "Resampled dataset shape ",
+#     paste(names(smote_train_y_categ), smote_train_y_categ, sep = ": ", collapse = ", ")
+#   )
+# )
+# smote_train_df$fraud_flag <- as.factor(smote_train_df$fraud_flag)
+# smote_train_y_categ
+
+# smote_train_df
+# glimpse(smote_train_df)
+
+
+
+# # Define the recipe
+# tree_recipe <- recipe(fraud_flag ~ ., data = smote_train_df) %>%
+#   step_dummy(all_nominal(), -all_outcomes()) %>%
+#   step_rm(all_outcomes())
+
+# # Preprocess the training data
+# preprocessed_train_data <- tree_recipe %>%
+#   prep(data = smote_train_df) %>%
+#   juice()
+
+# # Split the data into training and testing sets
+# # set.seed(123)
+# # split_data <- initial_split(preprocessed_train_data, prop = 0.7)
+# # train_data_split <- training(split_data)
+# # test_data_split <- testing(split_data)
+
+# # Define the random forest model
+# rf_model <- rand_forest() %>%
+#   set_engine("randomForest", importance = TRUE) %>%
+#   set_mode("classification")
+
+# # Define the workflow
+# rf_workflow <- workflow() %>%
+#   add_model(rf_model) %>%
+#   add_formula(fraud_flag ~ .)
+
+# # Train the model
+# rf_fit <- rf_workflow %>%
+#   fit(data = smote_train_df)
+
+# # Make predictions on the test set
+# rf_preds <- predict(rf_fit, new_data = test_data) %>%
+#   bind_cols(test_data)
+
+# # Evaluate the model
+# rf_metrics <- rf_preds %>%
+#   metrics(truth = fraud_flag, estimate = .pred_class)
+
+# # Print the evaluation metrics
+# rf_metrics
+
+
+
+
+
+# # Define the recipe
+# tree_recipe <- recipe(fraud_flag ~ ., data = smote_train_df) %>%
+#   step_dummy(all_nominal(), one_hot = TRUE)
+
+# # Define the model specification
+# tree_spec <- decision_tree(mode = "classification") %>%
+#   set_engine("rpart")
+
+# # Combine recipe and model specification into a workflow
+# tree_workflow <- workflow() %>%
+#   add_recipe(tree_recipe) %>%
+#   add_model(tree_spec)
+
+# # Train the model
+# tree_fit <- tree_workflow %>%
+#   fit(data = smote_train_df)
+
+# # Make predictions on the test set
+# predictions <- predict(tree_fit, test_data) %>%
+#   bind_cols(test_data)
+
+# # Evaluate the model
+# conf_matrix <- conf_mat(predictions, truth = fraud_flag, estimate = .pred_class)
+# print(conf_matrix)
